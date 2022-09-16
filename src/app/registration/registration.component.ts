ca825997cceaf41c6096f246e635b9da6eb19355
Reddy <beerreddy@deloitte.com>
2022-09-16 19:20:31 +0530

    registration ui

---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+
+  user: User=new User("","",0,"");
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
